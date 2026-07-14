42bf7355b29a65b4e8d4c442c66b72c12e9afb4e
dany-MG <dmanriqueg2000@alumno.ipn.mx>
2026-07-14 16:39:50 -0600

    Stage 0: hello server

--- BE-01/backend/main.py
+++ BE-01/backend/main.py
@@ -1,26 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI(
-    title = "BE-01",
-    description="A tiny backend with two JSON endpoints"
-)
+app = FastAPI()
 
-#Endpoint 1 
+
 @app.get("/")
-def read_root():
-    return{
-        "message": "Hello FlyRank",
-        "author" : "Daniel",
-        "version" : "1.0.0",
-        "ready_internship" : True
-    }
-
-#Endpoint 2
-@app.get("/api/intern")
-def read_intern():
-    return{
-        "message": "I hope I'll learn a lot during this internship",
-        "author" : "Daniel",
-        "version" : "1.0.0",
-    }
-
+async def root():
+    return {"message": "Hello World"}
