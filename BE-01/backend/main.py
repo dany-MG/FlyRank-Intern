f0a51ca98de88394950901fad3bd2d0c2f56a6f9
dany-MG <dmanriqueg2000@alumno.ipn.mx>
2026-07-17 20:35:02 -0600

    Stage 5: Swagger UI

--- BE-01/backend/main.py
+++ BE-01/backend/main.py
@@ -17,18 +17,22 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    """Root endpoint that returns basic information about the API."""
     return {"name" : "Task API", "version" : "1.0", "endpoints": ["/tasks"]}
 
 @app.get("/health")
 async def health():
+    """Health check endpoint that returns the status of the API."""
     return {"status" : "ok"}
 
 @app.get("/tasks")
 async def get_tasks():
+    """Endpoint to retrieve all tasks."""
     return {"tasks" : tasks}
 
 @app.get("/tasks/{task_id}")
 async def get_task(task_id:int):
+    """Endpoint to retrieve a specific task by its ID."""
     for task in tasks:
         if task.get("id") == task_id:
             return {"task" : task}
@@ -39,6 +43,7 @@ async def get_task(task_id:int):
 
 @app.post("/tasks", status_code=201)
 async def create_task(task: Task):
+    """Endpoint to create a new task. Validates that the task title is provided and not empty."""
     if task.title is None or task.title.strip() == "":
         raise HTTPException(
             status_code = 400,
@@ -52,6 +57,7 @@ async def create_task(task: Task):
 
 @app.put("/tasks/{id}")
 async def update_task(id:int, task:Task):
+    """Endpoint to update an existing task. Validates that the task title is not empty if provided."""
     if task.title is not None and task.title.strip() == "":
         raise HTTPException(
             status_code = 400,
@@ -73,6 +79,7 @@ async def update_task(id:int, task:Task):
             
 @app.delete("/tasks/{id}")
 async def delete_taks(id:int):
+    """Endpoint to delete a task by its ID."""
     for task in tasks:
         if task.get("id") == id:
             tasks.remove(task)
