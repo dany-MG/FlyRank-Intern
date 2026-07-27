0ef3f061998fa2cd45b76823aa9cc62cbf58b427
dany-MG <dmanriqueg2000@alumno.ipn.mx>
2026-07-27 10:37:51 -0600

    Stage 1: database read endpoints

--- BE-02/src/repositories/task_repository.py
+++ BE-02/src/repositories/task_repository.py
@@ -5,18 +5,28 @@ tasks = [
     {"id": 2, "title": "Workout", "done": True},
     {"id": 3, "title": "Walk the puppies", "done": False}
 ]"""
+import sqlite3
+
+def get_conn():
+    con = sqlite3.connect("./src/database/tasks.db")
+    con.row_factory = sqlite3.Row
+    return con
 
 class TasksRepository:
     @staticmethod
     def get_all_tasks():
-        return tasks
+        with get_conn() as con:
+            cur = con.cursor()
+            res = cur.execute("SELECT * FROM tasks")
+            return [dict(row) for row in res.fetchall()]
 
     @staticmethod
     def get_by_id(task_id: int):
-        for t in tasks:
-            if t["id"] == task_id:
-                return t
-        return None
+        with get_conn() as con:
+            cur = con.cursor()
+            res = cur.execute("SELECT id, title, done from tasks WHERE id = ?", (task_id,))
+            row = res.fetchone()
+            return dict(row) if row else None
 
     @staticmethod
     def create_task(new_task: dict):
